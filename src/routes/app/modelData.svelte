eadbb0220cb5d1ff186dff0505af02c364b03a03
Danilo Martins <danilo.m.a@gmail.com>
2022-03-23 16:19:41 -0300

    Initial commit

---
+++ src/routes/app/modelData.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let modelData;
+</script>
+
+<article>
+    <h1>{modelData.name}</h1>
+    <small>
+        
+    </small>
+</article>
---
+++ src/routes/app/modelList.svelte
@@ -0,0 +1,4 @@
+<script>
+    import { Mount } from "svelte";
+    import ModelData from "./ModelData.svelte";
+</script>
